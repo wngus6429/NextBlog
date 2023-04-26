c64831ef8aa78b8679497d257b38cdfa66bfdd53
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2023-04-26 19:41:56 +0900

    post bottom

--- blog/src/service/posts.ts
+++ blog/src/service/posts.ts
@@ -10,7 +10,7 @@ export type Post = {
   featured: boolean;
 };
 //! 타입을 정의하면서 기존 Post타입과 추가적으로 더해짐
-export type PostData = Post & { content: string };
+export type PostData = Post & { content: string; next: Post | null; prev: Post | null };
 
 export async function getFeaturedPosts(): Promise<Post[]> {
   return getAllPosts().then((posts) => posts.filter((post) => post.featured));
@@ -31,8 +31,12 @@ export async function getAllPosts(): Promise<Post[]> {
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
-  const metadata = await getAllPosts().then(posts => posts.find(post => post.path === fileName));
-  if(!metadata) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`)
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
+  if (!post) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null; // 최신이 숫자가 낮게 데이터 구조
+  const prev = index < posts.length ? posts[index + 1] : null;
   const content = await readFile(filePath, 'utf-8');
-  return { ...metadata, content};
+  return { ...post, content, next, prev };
 }
